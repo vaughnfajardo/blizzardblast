f3605f66fd5d78cab12cf0190861bfcdc165d23b
Vaughn Fajardo <40432972+vaughnfajardo@users.noreply.github.com>
2022-11-30 08:36:14 +0800

    added 3 views, created all links to all pages

--- blizzardblast/settings.py
+++ blizzardblast/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "orderprep",
 ]
 
 MIDDLEWARE = [
--- orderprep/views.py
+++ orderprep/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from django.views import View
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Hello! Order Prep View')
+
+class OrderPrepView(View):
+    def get(self, request):
+        return HttpResponse('OrderPrepView')
+
+class SalesView(View):
+    def get(self, request):
+        return HttpResponse('SalesView')
+
+class FormView(View):
+    def get(self, request):
+        return HttpResponse('FormView')
+
+
