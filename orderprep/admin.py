9feb984bb8c82475738d3837de065eaa4f05b523
Vaughn Fajardo <40432972+vaughnfajardo@users.noreply.github.com>
2022-12-01 20:13:59 +0800

    adjusted admin.py with changes to model

--- orderprep/admin.py
+++ orderprep/admin.py
@@ -1,20 +1,16 @@
 from django.contrib import admin
 
-from .models import Addon, AddonTally, Customer, Customization, Employee, Ingredient, Manager, Milkshake, MilkshakeSales, MilkshakeTally, Orders, Recipe, RecipeIngredient, RecipeTally, Restock, Staff
+from .models import Addon, Customer, Customization, Employee, Ingredient, Manager, Milkshake, Orders, Recipe, RecipeIngredient, Restock, Staff
 
 admin.site.register(Addon)
-admin.site.register(AddonTally) 
 admin.site.register(Customer)
 admin.site.register(Customization)
 admin.site.register(Employee)
 admin.site.register(Ingredient) 
 admin.site.register(Manager)
 admin.site.register(Milkshake)
-admin.site.register(MilkshakeSales) 
-admin.site.register(MilkshakeTally)
 admin.site.register(Orders) 
 admin.site.register(Recipe)
 admin.site.register(RecipeIngredient)
-admin.site.register(RecipeTally)
 admin.site.register(Restock) 
 admin.site.register(Staff)
